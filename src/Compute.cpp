4f268e4a1e6d3aea7dae9895ba4e146b305f5723
eithan <eithan.nakache@epita.fr>
2024-11-03 16:25:43 +0100

    fix lab distance

--- src/Compute.cpp
+++ src/Compute.cpp
@@ -39,65 +39,73 @@ void init_background_model(ImageView<rgb8> in)
   time_since_match = 0;
 }
 
-void RGBtoXYZ(uint8_t R, uint8_t G, uint8_t B, double& X, double& Y, double& Z) {
-    // Convertir de [0,255] à [0,1]
-    double r = R / 255.0;
-    double g = G / 255.0;
-    double b = B / 255.0;
 
-    // Convertir à l'espace linéaire
-    r = r > 0.04045 ? pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
-    g = g > 0.04045 ? pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
-    b = b > 0.04045 ? pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+struct Lab {
+    double L;
+    double a;
+    double b;
+};
 
-    // Utiliser les coefficients pour l'espace sRGB vers XYZ
-    X = r * 0.4124564 + g * 0.3575761 + b * 0.1804375;
-    Y = r * 0.2126729 + g * 0.7151522 + b * 0.0721750;
-    Z = r * 0.0193339 + g * 0.1191920 + b * 0.9503041;
+
+Lab rgb2lab(rgb8 rgb) {
+    double r = rgb.r / 255.0;
+    double g = rgb.g / 255.0;
+    double b = rgb.b / 255.0;
+
+    // Convert sRGB to linear RGB
+    r = (r > 0.04045) ? std::pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+    g = (g > 0.04045) ? std::pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+    b = (b > 0.04045) ? std::pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+
+    // Convert linear RGB to CIE XYZ
+    double X = r * 0.4124564 + g * 0.3575761 + b * 0.1804375;
+    double Y = r * 0.2126729 + g * 0.7151522 + b * 0.0721750;
+    double Z = r * 0.0193339 + g * 0.1191920 + b * 0.9503041;
+
+    // Convert CIE XYZ to CIE Lab
+    X /= 0.95047;
+    Y /= 1.0;
+    Z /= 1.08883;
+
+    X = (X > 0.008856) ? std::pow(X, 1.0 / 3.0) : (903.3 * X + 16) / 116.0;
+    Y = (Y > 0.008856) ? std::pow(Y, 1.0 / 3.0) : (903.3 * Y + 16) / 116.0;
+    Z = (Z > 0.008856) ? std::pow(Z, 1.0 / 3.0) : (903.3 * Z + 16) / 116.0;
+
+    return {
+        116.0 * Y - 16.0,
+        500.0 * (X - Y),
+        200.0 * (Y - Z)
+    };
 }
 
-void XYZtoLAB(double X, double Y, double Z, double& L, double& a, double& b) {
-    double xr = X / 95.047;
-    double yr = Y / 100.000;
-    double zr = Z / 108.883;
-
-    xr = xr > 0.008856 ? std::pow(xr, 1.0/3.0) : (7.787 * xr) + (16.0/116.0);
-    yr = yr > 0.008856 ? std::pow(yr, 1.0/3.0) : (7.787 * yr) + (16.0/116.0);
-    zr = zr > 0.008856 ? std::pow(zr, 1.0/3.0) : (7.787 * zr) + (16.0/116.0);
-
-    L = (116 * yr) - 16;
-    a = 500 * (xr - yr);
-    b = 200 * (yr - zr);
+double labDistance(const Lab& lab1, const Lab& lab2) {
+    double dL = lab1.L - lab2.L;
+    double da = lab1.a - lab2.a;
+    double db = lab1.b - lab2.b;
+    return std::sqrt(dL * dL + da * da + db * db);
 }
 
-double LABDistance(double L1, double a1, double b1, double L2, double a2, double b2) {
-    return std::sqrt((L2 - L1) * (L2 - L1) + (a2 - a1) * (a2 - a1) + (b2 - b1) * (b2 - b1));
-    //return sqrt(SQR(rgb_a.r - rgb_b.r) + SQR(rgb_a.g - rgb_b.g) + SQR(rgb_a.b - rgb_b.b));
-}
 
-double matchImagesLAB(ImageView<rgb8>& img1, ImageView<rgb8>& img2) {
-    if (img1.width != img2.width || img1.height != img2.height)
-        return false;
-
+double matchImagesLab(ImageView<rgb8>& img1, ImageView<rgb8>& img2) {
+    if (img1.width != img2.width || img1.height != img2.height) {
+        std::cout << "Error: images dimensions" << std::endl;
+        return -1.0;  // Retourne une valeur indicative d'erreur
+    }
+    
     double totalDistance = 0;
     int numPixels = 0;
 
     for (int y = 0; y < img1.height; ++y) {
         for (int x = 0; x < img1.width; ++x) {
-            //char* pixel1 = img1.buffer + y * img1.stride + x * 3;
             rgb8* pixel1 = img1.buffer + y * (img1.stride / sizeof(rgb8)) + x;
-            //char* pixel2 = img2.buffer + y * img2.stride + x * 3;
             rgb8* pixel2 = img2.buffer + y * (img2.stride / sizeof(rgb8)) + x;
 
-            double X1, Y1, Z1, L1, a1, b1;
-            double X2, Y2, Z2, L2, a2, b2;
+            // Convertir chaque pixel RGB en Lab
+            Lab lab1 = rgbToLab(*pixel1);
+            Lab lab2 = rgbToLab(*pixel2);
 
-            RGBtoXYZ(pixel1->r, pixel1->g, pixel1->b, X1, Y1, Z1);
-            XYZtoLAB(X1, Y1, Z1, L1, a1, b1);
-            RGBtoXYZ(pixel2->r, pixel2->g, pixel2->b, X2, Y2, Z2);
-            XYZtoLAB(X2, Y2, Z2, L2, a2, b2);
-
-            double distance = LABDistance(L1, a1, b1, L2, a2, b2);
+            // Calculer la distance Lab (ΔE)
+            double distance = labDistance(lab1, lab2);
             totalDistance += distance;
             numPixels++;
         }
@@ -155,7 +163,7 @@ void average(ImageView<rgb8>& img1, const ImageView<rgb8> img2) {
 }
 
 int background_estimation_process(ImageView<rgb8> in){
-  double match_distance = matchImagesRGB(bg_value, in);//matchImagesLAB(bg_value, in);
+  double match_distance = matchImagesLab(bg_value, in);//matchImagesLAB(bg_value, in);
 
   if (match_distance < 25){
     average(bg_value, in);
