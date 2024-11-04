9831987ec0a912e89c7f71c85f95a09c07829913
Your Name <you@example.com>
2024-11-04 17:08:42 +0100

    apply new filter v3

--- src/Compute.cpp
+++ src/Compute.cpp
@@ -254,7 +254,7 @@ int background_estimation_process(ImageView<rgb8> in){
       }
       time_since_match++;
     }
-    else if (time_since_match < 5){
+    else if (time_since_match < 80){
       average(candidate_value, in);
       time_since_match++;
     }
