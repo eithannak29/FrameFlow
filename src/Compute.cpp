a284e2d246dd17bb9eb95a9e5daa8c7f1228f313
Your Name <you@example.com>
2024-11-04 15:51:14 +0100

    fix tresh 2

--- src/Compute.cpp
+++ src/Compute.cpp
@@ -257,7 +257,7 @@ int background_estimation_process(ImageView<rgb8> in){
       }
       time_since_match++;
     }
-    else if (time_since_match < 3){
+    else if (time_since_match < 7){
       average(candidate_value, in);
       time_since_match++;
     }
