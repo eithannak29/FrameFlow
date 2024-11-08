79a290e8d173828497724aacd32a4d23d865da9d
Gautier Gally <gautier.gally@epita.fr>
2024-11-09 00:07:33 +0100

    todo: fixe erosion and compute kernel size

--- src/filter.cpp
+++ src/filter.cpp
@@ -48,7 +48,8 @@ void erode(ImageView<rgb8>& image, int radius) {
                 //std::cout << "(y: " << y << ", x: " << x << ")" << std::endl;
                 rgb8& pixel = copy.buffer[y * copy.width + x];
                 pixel.r = 0;
-                pixel.b = pixel.g;
+                pixel.g = 0;
+                pixel.b = 0;
             }
         }
         image = copy;  // Appliquer la copie modifiée à l'image d'origine
@@ -59,12 +60,12 @@ void erode(ImageView<rgb8>& image, int radius) {
 void dilate(ImageView<rgb8>& image, int radius) {
     ImageView<rgb8> copy = image;  // Faire une copie temporaire de l'image pour éviter la corruption
     int diameter = 2 * radius + 1;
-
+    rgb8& intensity = image.buffer[0];
     for (int y = radius; y < image.height - radius; ++y) {
         for (int x = radius; x < image.width - radius; ++x) {
             bool dilatation = false;
             rgb8& pixel = image.buffer[y * image.width + x];
-            if(pixel.b == 0){
+            if(pixel.r != 0){
                 continue;
             }
             for (int ky = 0; !dilatation && ky < diameter; ++ky) {
@@ -72,15 +73,17 @@ void dilate(ImageView<rgb8>& image, int radius) {
                     int ny = y + ky - radius;
                     int nx = x + kx - radius;
                     rgb8 kernel_pixel = image.buffer[ny * image.width + nx];
-                    if (kernel_pixel.b == 0) {
-                        dilatation = true;                        
+                    if (kernel_pixel.r != 0) {
+                        dilatation = true;
+                        intensity = kernel_pixel;
                     }
                 }
             }
             if (dilatation) {
                 rgb8& pixel = copy.buffer[y * copy.width + x];
-                pixel.r = pixel.g;
-                pixel.b = 0;
+                pixel.r = intensity.r;
+                pixel.g = intensity.g;
+                // pixel.b = 0;
             }
         }
     }
@@ -92,7 +95,7 @@ void dilate(ImageView<rgb8>& image, int radius) {
 void morphologicalOpening(ImageView<rgb8>& image, int radius) {
     // std::cout << "start morphologie"  << std::endl;
     // Étape 1 : Erosion
-    erode(image,  radius);
+    // erode(image,  radius);
     // std::cout << "dilatation" << std::endl;
     // Étape 2 : Dilatation
     dilate(image, radius);
