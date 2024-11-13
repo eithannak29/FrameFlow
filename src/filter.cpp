5b2124b8d09e16e5a47da80505a6eeba1298928c
baptiste.bellamy <baptiste.bellamy@epita.fr>
2024-11-13 20:53:10 +0100

    cpp test

--- src/filter.cpp
+++ src/filter.cpp
@@ -103,8 +103,8 @@ void morphologicalOpening(ImageView<rgb8> in, int minradius) {
 }
 
 ImageView<rgb8> HysteresisThreshold(ImageView<rgb8> in) {
-  const int lowThreshold = 20; 
-  const int highThreshold = 50;
+  const int lowThreshold = 50; 
+  const int highThreshold = 100;
 
   // Créer une queue pour propager les pixels de bord fort
   std::queue<std::pair<int, int>> edgeQueue;
