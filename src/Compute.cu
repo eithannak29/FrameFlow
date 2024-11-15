99f9ad23e050719bea578fd77f23c499161e5d72
baptiste.bellamy <baptiste.bellamy@epita.fr>
2024-11-15 12:25:31 +0100

    test

--- src/Compute.cu
+++ src/Compute.cu
@@ -402,9 +402,9 @@ void compute_cu(ImageView<rgb8> in)
 
     // Compute the radius for the kernel
     int min_dimension = std::min(in.width, in.height);
-    int ratio_disk = 1; // 1% of the smallest dimension
-    int minradius = 5;
-    int radius = std::max(minradius, (min_dimension / 100) * ratio_disk);
+    int ratio_disk = 2; // 1% of the smallest dimension
+    int minradius = 3;
+    int radius = std::max(minradius, (min_dimension / 100) / ratio_disk);
 
     // Create the disk kernel on the host
     int diameter = 2 * radius + 1;
