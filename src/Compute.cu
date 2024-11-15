7b695a822b89132ede238d3676c65848574ee9c4
baptiste.bellamy <baptiste.bellamy@epita.fr>
2024-11-15 12:33:26 +0100

    test

--- src/Compute.cu
+++ src/Compute.cu
@@ -459,7 +459,7 @@ void compute_cu(ImageView<rgb8> in)
     //cudaMemcpy2D(device_in.buffer, device_in.stride, copy.buffer, copy.stride, in.width * sizeof(rgb8), in.height, cudaMemcpyDeviceToDevice);
 
 
-    hysteresis<<<grid, block>>>(device_in, 5, 80);
+    hysteresis<<<grid, block>>>(device_in, 5, 50);
     //propagate_edges_process<<<grid, block>>>(device_in, 20, 50);
     cudaDeviceSynchronize();
 
