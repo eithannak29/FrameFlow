28b5414b5e50b91a4c302b8d4d38525696ddd35e
baptiste.bellamy <baptiste.bellamy@epita.fr>
2024-11-15 17:46:26 +0100

    cpp

--- src/Compute.cpp
+++ src/Compute.cpp
@@ -149,7 +149,7 @@ void compute_cpp(ImageView<rgb8> in)
     background_estimation_process(in);
     morphologicalOpening(in, 3);
 
-    ImageView<rgb8> mask = HysteresisThreshold(in, 25, 50);
+    ImageView<rgb8> mask = HysteresisThreshold(in, 15, 50);
     in = applyRedMask(in, mask, initialPixels);
     }
 
